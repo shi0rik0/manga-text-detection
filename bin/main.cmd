09c12bceaf267b9c1a8a0735cbe9b9b0c1b8e245
shi0rik0 <anguuan@outlook.com>
2025-04-16 15:34:37 +0800

    v0.1.2

--- bin/main.cmd
+++ bin/main.cmd
@@ -2,8 +2,6 @@
 setlocal
 
 cd /d %~dp0..\
-call .\venv\Scripts\activate.bat
-python -m manga_text_detection
-call .\venv\Scripts\deactivate.bat
+.\venv\Scripts\python.exe -m manga_text_detection
 
 endlocal
